f1657eeb96f98acfc21a5f79abf914ea1a7f8616
Vasudev Jaiswal <vasudevjaiswal050405@gmail.com>
2026-07-10 23:54:17 +0530

    feat: font and landing

--- app/page.tsx
+++ app/page.tsx
@@ -1,89 +1,60 @@
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { AlertCircle, ArrowUpRightIcon, Monitor, Users } from "lucide-react";
-import Link from "next/link";
-import { CustomRoomIdForm } from "./_components/custom-room-id-form";
-import { Badge } from "@/components/ui/badge";
-import { ThemeSwitch } from "@/components/theme-switch";
+"use client";
 
-export default function Home() {
+import Feature from "@/components/sections/feature";
+import { Footer } from "@/components/sections/footer";
+import { Header } from "@/components/sections/header";
+import { Hero } from "@/components/sections/hero";
+import { cn } from "@/lib/utils";
+import { motion } from "motion/react";
+import { useEffect, useState } from "react";
+
+const CONTENT_VARIANTS = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: { type: "spring", stiffness: 100, damping: 30 },
+  },
+} as const;
+
+export default function HomePage() {
+  const [transition, setTransition] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setTransition(true), 1250);
+    const timer2 = setTimeout(() => setIsLoaded(true), 2500);
+    return () => {
+      clearTimeout(timer);
+      clearTimeout(timer2);
+    };
+  }, []);
+
   return (
-    <div>
-      <div className="mx-auto flex h-[90vh] max-w-3xl flex-col justify-center gap-8 px-4 py-8">
-        <div className="flex flex-col items-center gap-4 text-center">
-          <div className="flex gap-2">
-            <Badge
-              render={
-                <a
-                  href="https://github.com/VA5UDEV/screen-sharing"
-                  target="_blank"
-                >
-                  Repo Link <ArrowUpRightIcon data-icon="inline-end" />
-                </a>
-              }
-            />
-            <Badge render={<ThemeSwitch iconSize={12} />} />
-          </div>
-          <h1 className="mx-auto max-w-3xl text-4xl font-bold tracking-tight text-balance sm:text-5xl">
-            Share Your Screen Instantly
-          </h1>
-          <p className="text-muted-foreground mx-auto max-w-xl text-xl text-balance">
-            Create, join, and share your screen with others in real-time.
-          </p>
-        </div>
-        <div className="mx-auto flex w-full max-w-3xl flex-col gap-8 px-4">
-          <Card>
-            <CardHeader>
-              <CardTitle className="flex items-center gap-2">
-                <Monitor size={20} />
-                Start Sharing
-              </CardTitle>
-              <CardDescription>
-                Create a room and share your screen with others
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="flex flex-col gap-4">
-              <Link href="/host">
-                <Button className="w-full">Create Room</Button>
-              </Link>
-              <CustomRoomIdForm />
-            </CardContent>
-          </Card>
-          <Card className="my-auto">
-            <CardHeader>
-              <CardTitle className="flex items-center gap-2">
-                <Users size={20} />
-                Join a Room
-              </CardTitle>
-              <CardDescription>
-                Enter a room code to view someone's screen
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <Link href="/join">
-                <Button variant="default" className="w-full">
-                  Join Room
-                </Button>
-              </Link>
-            </CardContent>
-          </Card>
-        </div>
+    <main className={cn("relative h-dvh", !isLoaded && "overflow-y-hidden")}>
+      <Header transition={transition} />
+
+      <div className="flex h-dvh w-full flex-col justify-between">
+        {transition && (
+          <>
+            <div>
+              <motion.div
+                variants={CONTENT_VARIANTS}
+                initial="hidden"
+                animate={transition ? "visible" : "hidden"}
+                className="w-full"
+              >
+                <Hero key={String(transition)} />
+              </motion.div>
+
+              <Feature />
+            </div>
+
+            <Footer />
+          </>
+        )}
       </div>
-      <Alert className="absolute bottom-0 w-full rounded-none border-none">
-        <AlertCircle />
-        <AlertTitle>Note</AlertTitle>
-        <AlertDescription>
-          Screen sharing isn't supported on mobile devices. Mobile users can
-          still join a room to view screens shared by others.
-        </AlertDescription>
-      </Alert>
-    </div>
+    </main>
   );
 }
