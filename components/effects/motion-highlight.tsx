f1c9e701216d06a38311f84596d0104e54486ce6
Vasudev Jaiswal <vasudevjaiswal050405@gmail.com>
2026-07-11 00:00:27 +0530

    feat: fix

--- components/effects/motion-highlight.tsx
+++ components/effects/motion-highlight.tsx
@@ -3,7 +3,7 @@
 import * as React from 'react';
 import { AnimatePresence, Transition, motion } from 'motion/react';
 
-import { cn } from '@workspace/ui/lib/utils';
+import { cn } from '@/lib/utils';
 
 type MotionHighlightMode = 'children' | 'parent';
 
