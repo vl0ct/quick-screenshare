d25619501ae4da53f40f0d6e51e4387a1d847c6b
Vasudev Jaiswal <vasudevjaiswal050405@gmail.com>
2026-07-10 23:57:11 +0530

    feat: icons

--- components/ui/icons/cast.tsx
+++ components/ui/icons/cast.tsx
@@ -8,7 +8,7 @@ import {
   useAnimateIconContext,
   IconWrapper,
   type IconProps,
-} from '@/components/animate-ui/icons/icon';
+} from '@/components/icons/icon';
 
 type CastProps = IconProps<keyof typeof animations>;
 
