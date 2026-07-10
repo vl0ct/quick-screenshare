ad64537a080e11cc9eadc6b48473ce27d94b9f66
Vasudev Jaiswal <vasudevjaiswal050405@gmail.com>
2026-07-11 02:49:41 +0530

    feat: font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
-import { inter, fontSerif, fontMono } from "./fonts";
+import { concertOne } from "./fonts";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
@@ -27,14 +27,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html
-      lang="en"
-      suppressHydrationWarning
-      className={cn("font-sans", inter.variable)}
-    >
-      <body
-        className={`${inter.variable} ${fontSerif.variable} ${fontMono.variable} antialiased`}
-      >
+    <html lang="en" suppressHydrationWarning className={concertOne.className}>
+      <body className={`flex min-h-screen flex-col antialiased`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
