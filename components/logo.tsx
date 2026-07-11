31fa02e552cfbcce7371431a4045710c6c325bbf
Vasudev Jaiswal <vasudevjaiswal050405@gmail.com>
2026-07-11 07:24:55 +0530

    fix

--- components/logo.tsx
+++ components/logo.tsx
@@ -35,7 +35,10 @@ const logoPaths = [
     d: "M 420 360 L 420.00006 360 A 59.99993999999998 59.99993999999998 0 0 1 480 419.99994 L 480 420 L 420 420 L 420 360 Z",
     stroke: false,
   },
-  { d: "M 300 360 L 360 360 L 360 420 L 300 420 Z", stroke: true },
+  {
+    d: "M 300 420 L 300 372 A 10 10 0 0 1 312 360 L 360 360 L 360 420 Z",
+    stroke: true,
+  },
   { d: "M 360 420 L 420 420 L 420 480 L 360 480 Z", stroke: true },
   { d: "M 420 420 L 480 420 L 480 480 L 420 480 Z", stroke: true },
   {
@@ -83,7 +86,7 @@ export const Logo = ({
             strokeWidth={path.stroke ? 20 : 0}
             strokeLinejoin="round"
             strokeLinecap="butt"
-            className="fill-neutral-900 dark:fill-neutral-100"
+            fill="currentColor"
             d={path.d}
           />
         ))}
